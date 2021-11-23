ed3d1838cbbb5956bffcc1d697e936f9cf509a86
Kutbeddin <korkmaz4851@gmail.com>
2021-11-24 00:33:20 +0300

    added

---
+++ student.js
@@ -0,0 +1,13 @@
+function Student(imgUrl,studentName,studentPath){
+    // console.log(this);
+    // console.log(self);
+     this.imgUrl = imgUrl;
+     this.studentPath = studentPath ;
+     this.studentName = studentName;   
+    //  console.log(this);
+}
+
+
+const std = new Student("url","ryan","fullstack")
+
+
